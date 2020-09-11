b81c3313a27593caf972a39d83de44161aa8657e
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-11 14:58:40 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Gurpreet Singh");
+$("#policyNumber").text("DN07861301");
+$("#startDate").text("12-Sep-2020");
+$("#endDate").text("11-Sep-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB03BB0958");
+$("#make").text("MAHINDRA");
+$("#model").text("JEEP");
+$("#vehicalSubType").text("CLASSIC");
+$("#engineNo").text("62847");
+$("#chassis").text("11C56245");
